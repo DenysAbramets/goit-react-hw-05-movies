573a326dca7e477e43bd1ff44dcab00f7c57702f
DenysAbramets <115029520+DenysAbramets@users.noreply.github.com>
2023-05-27 10:00:26 +0000

    Deploying to gh-pages from @ DenysAbramets/goit-react-hw-05-movies@fa892542c35a9cff6fe410f7f3068b47be048652 🚀

---
+++ static/js/545.18f9564f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[545],{545:function(e,t,n){n.r(t);var r=n(87),a=n(184);t.default=function(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h2",{children:"Page doesn't exist!"}),(0,a.jsx)("button",{children:(0,a.jsx)(r.rU,{to:"/",children:"Back to main page"})})]})}}}]);
+//# sourceMappingURL=545.18f9564f.chunk.js.map
